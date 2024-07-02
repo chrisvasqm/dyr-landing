943e664b68ae2e447aa61c82495e9e3cac6e4016
chrisvasqm <christianvasquezmelo@gmail.com>
2024-07-02 16:40:18 -0400

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
-    },
-  },
+  server: {
+    open: true,
+    port: 3000
+  }
 });
